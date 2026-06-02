2860f1ebb36e3e0d4f0f8780fa132f18f1a87143
SaadioFongangRoosevelt <rooseveltsaadio237@gmail.com>
2026-06-02 10:48:12 +0100

    Jalon 2 termine remplissage de la base de donnees

---
+++ seed.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Question = require("./models/Question");
+
+dotenv.config();
+
+const questions = [
+  {
+    category: "Manga",
+    text: "Qui est Naruto ?",
+    options: ["Ninja", "Pirate", "Samouraï"],
+    correctAnswer: "Ninja"
+  },
+  {
+    category: "F1",
+    text: "Qui est champion 2021 ?",
+    options: ["Hamilton", "Verstappen"],
+    correctAnswer: "Verstappen"
+  }
+];
+
+const seedDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+
+  await Question.deleteMany();
+  await Question.insertMany(questions);
+
+  console.log("DB remplie !");
+  process.exit();
+};
+
+seedDB();
